87b84d8370daef3c0ac6b5049e2c1d45e52353e7
danii <daniil.kravchenko.92@yandex.ru>
2021-12-27 15:21:28 +0300

    deleted files + added gitignore

--- src/test/java/com/daniil/StudentRegistrationForm.java
+++ src/test/java/com/daniil/StudentRegistrationForm.java
@@ -7,22 +7,16 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 @DisplayName("Тест второго урока")
 public class StudentRegistrationForm {
-
     @BeforeAll
     static void beforeAll(){
         Configuration.browserSize = "1920x1080";
     }
-
-
-
     @Test
     void studentRegistrationFormTest(){
         open("https://demoqa.com/automation-practice-form");
@@ -30,37 +24,17 @@ public class StudentRegistrationForm {
         $("#firstName").setValue("Daniil");
         $("#lastName").setValue("Kravchenko");
         $("#userEmail").setValue("secret@mail.com");
-        $("[for='gender-radio-1']").click(); //Не моя часть кода
-        // $("#gender-radi0-3").parent().click(); можно еще вот так
-        // $(byText("Other")).click(); bad practice
-        // $("#genterWrapper").$(byText("Other")).click(); Так лучше вместо обычного byText, с указанием в каком блоке наш текст
+        $("[for='gender-radio-1']").click();
         $("#userNumber").setValue("8999696969");
-        // $("#dateOfBirthInput").setValue("06 Jun 1992").pressEnter(); нераблчий вариант
-        //$(".react-datepicker__month-select").selectOption("June");
-        //$(".react-datepicker__year-select").selectOption("1992");
-        //$(".react-datepicker__day--06:not(.react-datepicker__day--outside-month)").click();
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").click();
         $("[value='6']").click();
         $(".react-datepicker__year-select").click();
         $("[value='1992']").click();
         $(".react-datepicker__day--006").click();
-
-        // :not() запись для поиска элемента исключая что-то
-        //        $$(".react-datepicker__day--06") Можно еще вот так
-        //        .filter(not(cssClass(".react-datepicker__day--outside-month")))
-        //        .first() // or .get(0)
-        //        .click();
-        // $x("//*[contains(@aria-label, \"June 6th, 1992\"]+"); Xpath лучше для поиска по сокращенному поиску, но более громоздкий
-        // $("[aria-label=\"Choose Wednesday, June 6th, 1992\"]"); CSS
-        // $("[aria-label$='June 6th, 1992']") CSS the best way; $ - starts with
-        // $("[aria-label*='June 6th']") CSS the best way also; * - contains
         $("#subjectsInput").setValue("History").pressEnter();
-        // можно еще setValue("H") byText и click() + может быть через hover(
         $("[for='hobbies-checkbox-1']").click();
-        //$("#uploadPicture").uploadFile(new File("src/test/resources/images/1.png"));
-        $("#uploadPicture").uploadFromClasspath("images/1.jpg"); // более простой способ
-
+        $("#uploadPicture").uploadFromClasspath("images/1.jpg");
         $("#currentAddress").setValue("Mohadisha Temple").scrollTo();
         $("#state").scrollTo().click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
@@ -80,14 +54,5 @@ public class StudentRegistrationForm {
         $(byText("Mohadisha Temple")).shouldBe(visible);
         $(byText("NCR")).shouldBe(visible);
         $(byText("Delhi")).shouldBe(visible);
-
-
-
-
-
-
-
     }
-
-
 }
